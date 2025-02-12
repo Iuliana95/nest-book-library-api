e245eb4f33f8da0d837f4498dd10a57a33368779
iuliana.dragoi <iulianadragoi95@gmail.com>
2025-02-12 20:07:26 +0200

    feat: Add SwaggerHub integration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+      .setTitle('Books API')
+      .setDescription('API for managing books and categories')
+      .setVersion('1.0')
+      .addTag('books')
+      .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, documentFactory);
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
