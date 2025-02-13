ac8809446aab4a672fe4d2931ac6130e1bb3cbb6
iuliana.dragoi <iulianadragoi95@gmail.com>
2025-02-13 20:34:36 +0200

    feat: Add global validation pipe for DTOs, and fix Pagination

--- src/books/dto/pagination.dto.ts
+++ src/books/dto/pagination.dto.ts
@@ -1,14 +1,17 @@
 import {IsNumber, IsOptional, IsPositive} from "class-validator";
+import {Transform} from "class-transformer";
 
 export class PaginationDto {
 
     @IsNumber()
     @IsPositive()
     @IsOptional()
+    @Transform(({ value }) => Number(value))
     skip: number;
 
     @IsNumber()
     @IsPositive()
     @IsOptional()
+    @Transform(({ value }) => Number(value))
     limit: number;
 }
